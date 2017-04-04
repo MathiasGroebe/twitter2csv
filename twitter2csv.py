85fdec9519cd8d53b0a55beb9435ed9cf7b107d0
Mathias Gröbe M.Sc <mathias.groebe@tu-dresden.de>
2017-04-04 13:36:56 +0200

    Writing CSV

--- twitter2csv.py
+++ twitter2csv.py
@@ -7,9 +7,24 @@ import json
 
 #---setting variables---
 
-file_name = 'test3.json'
+in_file = 'test3.json'
+out_file = 'out.csv'
+delimiter = ';'
 
-with open(file_name, 'r', encoding="utf8") as data_file:
+csv_file = open(out_file, 'w', encoding = "utf8")
+csv_file.write('id' + delimiter + 'user_id' + delimiter + 'text' + delimiter + 'retweet_count' + delimiter + 'favorite_count' + delimiter + 'created_at' + delimiter + 'user_screen_name' + '\n')
+
+with open(in_file, 'r', encoding = "utf8") as data_file:
     for line in data_file:
         tweet = json.loads(line)
         print(tweet['id'])
+        csv_file.write(str(tweet['id']) + delimiter)
+        csv_file.write(str(tweet['user']['id']) + delimiter)
+        csv_file.write('"' + str(tweet['text']) + '"' + delimiter)
+        csv_file.write(str(tweet['retweet_count']) + delimiter)
+        csv_file.write(str(tweet['favorite_count']) + delimiter)
+        csv_file.write(str(tweet['created_at']) + delimiter)
+        csv_file.write(str(tweet['user']['screen_name']) + delimiter)
+        csv_file.write('\n')
+
+csv_file.close()
